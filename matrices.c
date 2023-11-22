25c49fff7350df57f3684f533b4e4524b5ba6ea1
IngGiova <giovanny.padilla@udla.edu.ec>
2023-11-22 16:08:12 -0500

    Creacion de variables

---
+++ matrices.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int fila, columna, i, matriz;
+    printf("Ingrese el numero de filas\n");
+    scanf("%d", &fila);
+    printf("Ingrese el numero de columnas\n");
+    scanf("%d", &columna);
+    
+    return 0;
+}
