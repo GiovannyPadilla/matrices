8af1204ab1a64cd996e1234bf62895444136100a
IngGiova <giovanny.padilla@udla.edu.ec>
2023-11-22 16:25:01 -0500

    Crear la matriz por fila y columna

--- matrices.c
+++ matrices.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    int fila, columna, i, matriz;
+int main() {
+    int filas, columna;
     printf("Ingrese el numero de filas\n");
-    scanf("%d", &fila);
+    scanf("%d", &filas);
     printf("Ingrese el numero de columnas\n");
     scanf("%d", &columna);
-    
+
+    int matriz[columna][filas];
+
+    // Crear la matriz por fila y columna
+    int cont = 1;
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columna; j++) {
+            matriz[j][i] = cont++;
+        }
+    }
     return 0;
 }
