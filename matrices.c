5276fbaac419ed5c01499d1fcf40ba08ef057ccd
IngGiova <giovanny.padilla@udla.edu.ec>
2023-11-22 16:27:04 -0500

    impresion de la matriz

--- matrices.c
+++ matrices.c
@@ -11,10 +11,16 @@ int main() {
 
     // Crear la matriz por fila y columna
     int cont = 1;
+    for (int m = 0; m < filas; m++) {
+        for (int n = 0; n < columna; n++) {
+            matriz[n][m] = cont++;
+        }
+    }
+    // Imprimir la matriz
+    printf("Matriz resultante:\n");
     for (int i = 0; i < filas; i++) {
         for (int j = 0; j < columna; j++) {
-            matriz[j][i] = cont++;
-        }
+            printf("%d ", matriz[j][i]);
     }
     return 0;
 }
